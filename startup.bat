8c500c7c7c7d38b49bab033efea9522b7d4dd851
a-MrX <129958350+a-MrX@users.noreply.github.com>
2023-04-05 23:32:40 +0800

    Update startup.bat

--- startup.bat
+++ startup.bat
@@ -1,8 +1,5 @@
 @echo off
 color 04
-
-powershell -Command Invoke-WebRequest "https://github.com/a-MrX/Backtrack/archive/refs/heads/main.zip" -OutFile main.zip
-powershell -Command Expand-Archive main.zip
 echo BBBBBB         AA
 echo BB    BB      AAAA
 echo BBB   BB     AAAAAA
