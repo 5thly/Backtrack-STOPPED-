3fc22200f7a78433a3fe6ffa9a94523779de77ba
a-MrX <129958350+a-MrX@users.noreply.github.com>
2023-04-06 13:52:40 +0800

    Add files via upload

--- index.bat
+++ index.bat
@@ -1,17 +1,16 @@
 @echo off
-rem // created by M.Are5th
+rem // R1 terminal
+rem // created by MrX
 rem // Open-source program for penter
 rem // You can change the configuration in penter.conf
 setlocal enabledelayedexpansion
 
 rem // update utf-8 symbol support
-
 :first
 set ip_address_string="IPv4 Address"
 set "tele="
 title "Backtrack Beta Version Tele-terminal 1.0.1 for pentest"
-color 04
-set /p "tele=Backtrack@MRX:~ "
+set /p "tele=BackTrack@R1:~ "
 option
 
 rem // testing/prototype command
