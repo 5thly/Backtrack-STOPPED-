73aa1029a13bf3d3283c96c3234a10b8d61b6e98
a-MrX <129958350+a-MrX@users.noreply.github.com>
2023-04-06 13:43:39 +0800

    Add files via upload

--- option.bat
+++ option.bat
@@ -6,6 +6,7 @@ if "%tele%" == "public-ip" "command/publicip"
 if "%tele%" == "clear" "command/cls"
 if "%tele:~0,4%" == "pico" "command/pico"
 if "%tele:~0,2%" == "ls" "command/ls"
+if "%tele:~0,3%" == "psh" "command/psh"
 if "%tele%" == "" call index.bat
 if not "%tele%" == "ip" "command/error"
 call first.bat
