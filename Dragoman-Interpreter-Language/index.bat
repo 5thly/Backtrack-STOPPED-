39dc8fb141426b3005c2525a688a03af0863f457
匠 <129958350+5thly@users.noreply.github.com>
2023-09-16 14:16:19 +0800

    Add files via upload

--- Dragoman-Interpreter-Language/index.bat
+++ Dragoman-Interpreter-Language/index.bat
@@ -1,5 +1,10 @@
 @echo off
+rem Pastikan anda menetapkan directory di bahagian bawah.
+rem Initdir adalah tempat directory init.bat.
+rem fildir adalah tempat directory interpreter.bat.
+set initdir=""
+set fildir=""
 :re
 set /p arads="Filename: "
-call C:\Users\akmal\Documents\Coding\Penter\init.bat %arads%
+call %initdir%/init.bat %arads%
 goto re
